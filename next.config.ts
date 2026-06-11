6127b71d8f803e0ea5dc820490cd39bcf653991a
Gopal Ojha <gopalojha913@gmail.com>
2026-06-11 15:47:39 +0000

    ## Error Type Runtime Error
    ## Error Message
    Body exceeded 1 MB limit.

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   serverActions: {
-    bodySizeLimit: '60mb',
+    bodySizeLimit: '100mb',
   },
   images: {
     remotePatterns: [
